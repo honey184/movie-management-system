8c0fd31744ada1b3b319ac786a33306b7919a063
Honey Ramani <honeyramani@team.bigscal.com>
2026-04-01 17:49:54 +0530

    add morgan

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const passport = require('./config/passport');
 const errorMiddleware = require('./middlewares/error.middleware');
 const routes = require('./routes/index.routes');
 const swagger = require('swagger-ui-express')
+const morgan = require('morgan');
 const swaggerSpec = require('./config/swagger')
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
 
